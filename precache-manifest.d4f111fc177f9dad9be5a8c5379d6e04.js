b8b3ca3c048e266ff3e7c9a17a74ebf3e7f92adf
liad <liadbenmoshe10@gmail.com>
2020-10-24 12:36:59 +0300

    Updates

---
+++ precache-manifest.d4f111fc177f9dad9be5a8c5379d6e04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c10049fe890cc4ede156bba71afed9",
+    "url": "/LiadBenMoshe/MySearchPhoto/index.html"
+  },
+  {
+    "revision": "c36ddfa751504b50077a",
+    "url": "/LiadBenMoshe/MySearchPhoto/static/css/main.5ad53e67.chunk.css"
+  },
+  {
+    "revision": "83780c69b5c5574b9273",
+    "url": "/LiadBenMoshe/MySearchPhoto/static/js/2.e752a839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/LiadBenMoshe/MySearchPhoto/static/js/2.e752a839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c36ddfa751504b50077a",
+    "url": "/LiadBenMoshe/MySearchPhoto/static/js/main.971f3323.chunk.js"
+  },
+  {
+    "revision": "74cbcd54f458c14b2701",
+    "url": "/LiadBenMoshe/MySearchPhoto/static/js/runtime-main.2941539a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/LiadBenMoshe/MySearchPhoto/precache-manifest.d4f111fc177f9dad9be5a8c5379d6e04.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LiadBenMoshe/MySearchPhoto/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
