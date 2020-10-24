897d26f28b2146eb9fa148a61dde2676cf25f9a3
liad <liadbenmoshe10@gmail.com>
2020-10-24 19:36:55 +0300

    build3

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
       <div className="ui segment">
         <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
-            <label>Search</label>
+            <label>Search Photo</label>
             <input
               type="text"
               value={this.state.term}
